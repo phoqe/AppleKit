7750bb8ec10b0ae44122836ed972a74dbf678e8d
Linus Långberg <git@phoqe.dev>
2021-10-07 19:29:06 +0200

    docs: describe closesheet

--- Sources/AppleKit/UI/Components/CloseSheetImage.swift
+++ Sources/AppleKit/UI/Components/CloseSheetImage.swift
@@ -1,12 +1,19 @@
 #if os(iOS)
 import SwiftUI
 
+/**
+ A stylized SF Symbol that appears in apps by Apple, e.g., Find My and Maps.
+ You can use it in multiple environments but more specifically as a close button in a Sheet.
+
+ - Requires: The symbol is only available on the iOS platform.
+ - Remark: The SF Symbol in use is `xmark.circle.fill`.
+ */
 public struct CloseSheetImage: View {
-    public init() {
-        
-    }
+    /// An empty initializer is required when importing AppleKit for use in other projects.
+    public init() {}
 
     public var body: some View {
+        // TODO: Add support for iOS 14 and below.
         Image(systemName: "xmark.circle.fill")
             .accessibilityLabel("Close")
             .symbolRenderingMode(.palette)
