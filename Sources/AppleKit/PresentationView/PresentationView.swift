02e476466bdca8e18802096d128ca19c3d8e8514
Linus Långberg <phoqe@phoqe.me>
2023-08-07 22:53:17 +0200

    improve presentationview

--- Sources/AppleKit/PresentationView/PresentationView.swift
+++ Sources/AppleKit/PresentationView/PresentationView.swift
@@ -8,7 +8,7 @@ struct PresentationView: View {
         let action: () -> Void
     }
     
-    let systemIcon: String?
+    let systemImage: String?
     
     let title: LocalizedStringKey
     let subtitle: LocalizedStringKey?
@@ -19,7 +19,7 @@ struct PresentationView: View {
     let secondaryButton: PresentationView.Button?
     
     init(
-        systemIcon: String? = nil,
+        systemImage: String? = nil,
         
         title: LocalizedStringKey,
         subtitle: LocalizedStringKey? = nil,
@@ -29,7 +29,7 @@ struct PresentationView: View {
         primaryButton: PresentationView.Button? = nil,
         secondaryButton: PresentationView.Button? = nil
     ) {
-        self.systemIcon = systemIcon
+        self.systemImage = systemImage
         
         self.title = title
         self.subtitle = subtitle
@@ -40,46 +40,54 @@ struct PresentationView: View {
         self.secondaryButton = secondaryButton
     }
     
-    var doubleLargeTitle: Font {
-        Font.system(size: UIFont.preferredFont(forTextStyle: .largeTitle).pointSize * 2)
+    var showOverlay: Bool {
+        disclaimer != nil || primaryButton != nil || secondaryButton != nil
     }
     
     var body: some View {
-        VStack(alignment: .center) {
-            ScrollView {
-                Group {
-                    VStack(alignment: .center, spacing: 25) {
-                        VStack(alignment: .center, spacing: 25) {
-                            if let systemIcon {
-                                Image(systemName: systemIcon)
-                                    .font(doubleLargeTitle)
-                                    .foregroundColor(.accentColor)
-                            }
-                            
-                            Text(title)
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                            
-                            if let subtitle {
-                                Text(subtitle)
-                                    .font(.body)
-                            }
+        ZStack {
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .center, spacing: 25) {
+                    Group {
+                        if let systemImage {
+                            Image(systemName: systemImage)
+                                .font(.system(size: UIFont.preferredFont(forTextStyle: .largeTitle).pointSize * 2))
+                                .foregroundColor(.accentColor)
                         }
-                        .multilineTextAlignment(.center)
                         
-                        if let text {
-                            Text(text)
-                                .font(.callout)
+                        Text(title)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        
+                        if let subtitle {
+                            Text(subtitle)
+                                .font(.body)
                         }
                     }
-                    .padding(.vertical, 50)
-                }
-                .padding(.horizontal, 50)
-            }
+                    .multilineTextAlignment(.center)
                     
-            if disclaimer != nil || primaryButton != nil || secondaryButton != nil {
-                Spacer()
-                
+                    if let text {
+                        Text(text)
+                            .font(.callout)
+                    }
+                }
+                .padding(.top, 50)
+                .padding(.horizontal, 50)
+                .if(showOverlay) { view in
+                    view.padding(.bottom, 125)
+                }
+                .if(!showOverlay) { view in
+                    view.padding(.bottom, 50)
+                }
+            }
+            .if(showOverlay) { view in
+                view.mask {
+                    LinearGradient(gradient: Gradient(colors: [.primary, .primary.opacity(0)]), startPoint: UnitPoint(x: 0.5, y: 0.85), endPoint: .bottom)
+                }
+            }
+        }
+        .if(showOverlay) { view in
+            view.overlay(alignment: .bottom) {
                 VStack(alignment: .center, spacing: 25) {
                     if let disclaimer {
                         Text(disclaimer)
@@ -103,7 +111,6 @@ struct PresentationView: View {
                             .fontWeight(.semibold)
                     }
                 }
-                .padding(.vertical, 15)
                 .padding(.horizontal, 50)
                 .multilineTextAlignment(.center)
             }
@@ -114,11 +121,10 @@ struct PresentationView: View {
 @available(iOS 16.0, *)
 struct PresentationView_Previews: PreviewProvider {
     static var previews: some View {
-        // Full
         Text("Presentation")
             .sheet(isPresented: .constant(true)) {
                 PresentationView(
-                    systemIcon: "minus.plus.batteryblock.fill",
+                    systemImage: "minus.plus.batteryblock.fill",
                     title: "What’s New",
                     subtitle: "Circular imports occur in Python when two or more modules depend on each other. This can create a problem because the modules cannot be loaded properly, and Python will raise an error.",
                     disclaimer: "Circular imports occur in Python when two or more modules depend on each other. This can create a problem because the modules cannot be loaded properly, and Python will raise an error.",
@@ -131,18 +137,32 @@ struct PresentationView_Previews: PreviewProvider {
                 )
             }
         
-        // Text
         Text("Presentation")
             .sheet(isPresented: .constant(true)) {
                 PresentationView(
-                    systemIcon: "minus.plus.batteryblock.fill",
+                    systemImage: "minus.plus.batteryblock.fill",
                     title: "What’s New",
                     subtitle: "Circular imports occur in Python when two or more modules depend on each other. This can create a problem because the modules cannot be loaded properly, and Python will raise an error.",
                     text: "To enable users to participate in actions that require a Hacker News account, such as upvoting a story or submitting a comment, the app automatically persists the user’s login to the official site in the background.\n\nThis is achieved through the use of HTTPS, which is a secure communication protocol that encrypts the data transmitted between the app and the official site. The encrypted data includes the user’s login information, ensuring that it remains confidential and protected against unauthorized access.\n\nBy using HTTPS, the app provides an additional layer of security compared to a standard connection, as it protects the user’s login information from being intercepted and viewed by third parties. The app’s implementation of HTTPS also meets industry standards for secure communication and is widely used by organizations and websites to protect sensitive information.\n\nMoreover, the app eliminates the need for the user to enter their login information repeatedly, making the app more convenient and efficient. The app’s persistent login feature ensures that users can participate in actions on Hacker News with ease and peace of mind, knowing that their login information is secure."
                 )
             }
         
-        // Required
+        Text("Presentation")
+            .sheet(isPresented: .constant(true)) {
+                PresentationView(
+                    systemImage: "minus.plus.batteryblock.fill",
+                    title: "What’s New",
+                    subtitle: "Circular imports occur in Python when two or more modules depend on each other. This can create a problem because the modules cannot be loaded properly, and Python will raise an error.",
+                    text: "To enable users to participate in actions that require a Hacker News account, such as upvoting a story or submitting a comment, the app automatically persists the user’s login to the official site in the background.\n\nThis is achieved through the use of HTTPS, which is a secure communication protocol that encrypts the data transmitted between the app and the official site. The encrypted data includes the user’s login information, ensuring that it remains confidential and protected against unauthorized access.\n\nBy using HTTPS, the app provides an additional layer of security compared to a standard connection, as it protects the user’s login information from being intercepted and viewed by third parties. The app’s implementation of HTTPS also meets industry standards for secure communication and is widely used by organizations and websites to protect sensitive information.\n\nMoreover, the app eliminates the need for the user to enter their login information repeatedly, making the app more convenient and efficient. The app’s persistent login feature ensures that users can participate in actions on Hacker News with ease and peace of mind, knowing that their login information is secure.",
+                    primaryButton: .init(title: "Continue", action: {
+                        
+                    }),
+                    secondaryButton: .init(title: "Set Up Later in Settings", action: {
+                        
+                    })
+                )
+            }
+        
         Text("Presentation")
             .sheet(isPresented: .constant(true)) {
                 PresentationView(
@@ -150,11 +170,10 @@ struct PresentationView_Previews: PreviewProvider {
                 )
             }
         
-        // Info
         Text("Presentation")
             .sheet(isPresented: .constant(true)) {
                 PresentationView(
-                    systemIcon: "person.and.background.dotted",
+                    systemImage: "person.and.background.dotted",
                     title: "Data & Privacy",
                     subtitle: "This icon appears when an Apple feature asks to use vour personal information.\n\nYou won't see this with every feature since Apple collects this information only when needed to enable features, secure our services or personalise your experience.\n\nApple believes privacy is a fundamental human right, so every Apple product is designed to minimise the collection and use of your data, use on-device processing whenever possible, and provide transparency and control over your information."
                 )
