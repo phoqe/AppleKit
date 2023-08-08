be88f8f880c121d49e694df1057a182b647b5de9
Linus Långberg <phoqe@phoqe.me>
2023-08-08 14:55:21 +0200

    test

--- Sources/AppleKit/Onboarding/OnboardingView.swift
+++ Sources/AppleKit/Onboarding/OnboardingView.swift
@@ -1,19 +1,56 @@
 import SwiftUI
 
 @available(iOS 15.0, *)
-public struct OnboardingView: View {
+class OnboardingViewModel: ObservableObject {
+    
+    let dismiss: DismissAction
+    let numberOfTabs: Int
+    
+    init(
+        dismiss: DismissAction,
+        numberOfTabs: Int
+    ) {
+        self.dismiss = dismiss
+        self.numberOfTabs = numberOfTabs
+    }
+    
+    @Published
+    var currentTab: Int = 0
+    
+    func nextTab() {
+        if currentTab < numberOfTabs - 1 {
+            currentTab = currentTab + 1
+        } else {
+            finish()
+        }
+    }
+    
+    func previousTab() {
+        if currentTab > 0 {
+            currentTab = currentTab - 1
+        }
+    }
+    
+    func finish() {
+        dismiss()
+    }
+}
+
+@available(iOS 15.0, *)
+public struct OnboardingView<Content: View>: View {
+    
+    private let tabs: [Content]
     
     @StateObject
     private var viewModel: OnboardingViewModel
     
-    private let tabs: [AnyView]
-    
     public init(
-        tabs: [AnyView]
+        tabs: [Content]
     ) {
         self._viewModel = StateObject(
             wrappedValue: OnboardingViewModel(
-                dismiss: Environment(\.dismiss).wrappedValue
+                dismiss: Environment(\.dismiss).wrappedValue,
+                numberOfTabs: tabs.count
             )
         )
         
@@ -22,7 +59,7 @@ public struct OnboardingView: View {
     
     public var body: some View {
         TabView(selection: $viewModel.currentTab) {
-            ForEach(0..<tabs.count, id: \.self) { index in
+            ForEach(tabs.indices, id: \.self) { index in
                 tabs[index]
                     .tag(index)
             }
@@ -37,7 +74,8 @@ struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         Text("Onboarding")
             .sheet(isPresented: .constant(true)) {
-                OnboardingView(tabs: [AnyView(Text("1")), AnyView(Text("2"))])
+                OnboardingView(tabs: [Text("1"), Text("2"), Text("3")])
             }
     }
 }
+
