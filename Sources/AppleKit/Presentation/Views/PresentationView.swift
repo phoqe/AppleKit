fe9886377ed3db0e336a40086162b0bd6991297c
Linus Långberg <phoqe@phoqe.me>
2023-08-09 12:02:58 +0200

    merge obview to pview

--- Sources/AppleKit/Presentation/Views/PresentationView.swift
+++ Sources/AppleKit/Presentation/Views/PresentationView.swift
@@ -2,7 +2,7 @@
 import SwiftUI
 
 @available(iOS 16.0, *)
-public struct PresentationView: View {
+public struct PresentationView<Content: View>: View {
     let systemImage: String?
     
     let title: LocalizedStringKey
@@ -13,6 +13,9 @@ public struct PresentationView: View {
     let primaryButton: PresentationButton?
     let secondaryButton: PresentationButton?
     
+    @ViewBuilder
+    let content: Content
+    
     public init(
         systemImage: String? = nil,
         
@@ -22,7 +25,10 @@ public struct PresentationView: View {
         
         disclaimer: LocalizedStringKey? = nil,
         primaryButton: PresentationButton? = nil,
-        secondaryButton: PresentationButton? = nil
+        secondaryButton: PresentationButton? = nil,
+        
+        @ViewBuilder
+        content: () -> Content
     ) {
         self.systemImage = systemImage
         
@@ -33,6 +39,8 @@ public struct PresentationView: View {
         self.disclaimer = disclaimer
         self.primaryButton = primaryButton
         self.secondaryButton = secondaryButton
+        
+        self.content = content()
     }
     
     var showOverlay: Bool {
@@ -64,6 +72,8 @@ public struct PresentationView: View {
                     if let text {
                         Text(text)
                             .font(.callout)
+                    } else {
+                        content
                     }
                 }
                 .padding(.top, 50)
@@ -109,7 +119,9 @@ struct PresentationView_Previews: PreviewProvider {
                     secondaryButton: .init(title: "Set Up Later in Settings", action: {
                         
                     })
-                )
+                ) {
+                    
+                }
             }
         
         Text("Presentation")
@@ -122,7 +134,9 @@ struct PresentationView_Previews: PreviewProvider {
                     primaryButton: .init(title: "Continue", action: {
                         
                     })
-                )
+                ) {
+                    
+                }
             }
         
         Text("Presentation")
@@ -132,7 +146,9 @@ struct PresentationView_Previews: PreviewProvider {
                     title: "What’s New",
                     subtitle: "Circular imports occur in Python when two or more modules depend on each other. This can create a problem because the modules cannot be loaded properly, and Python will raise an error.",
                     text: "To enable users to participate in actions that require a Hacker News account, such as upvoting a story or submitting a comment, the app automatically persists the user’s login to the official site in the background.\n\nThis is achieved through the use of HTTPS, which is a secure communication protocol that encrypts the data transmitted between the app and the official site. The encrypted data includes the user’s login information, ensuring that it remains confidential and protected against unauthorized access.\n\nBy using HTTPS, the app provides an additional layer of security compared to a standard connection, as it protects the user’s login information from being intercepted and viewed by third parties. The app’s implementation of HTTPS also meets industry standards for secure communication and is widely used by organizations and websites to protect sensitive information.\n\nMoreover, the app eliminates the need for the user to enter their login information repeatedly, making the app more convenient and efficient. The app’s persistent login feature ensures that users can participate in actions on Hacker News with ease and peace of mind, knowing that their login information is secure."
-                )
+                ) {
+                    
+                }
             }
         
         Text("Presentation")
@@ -148,14 +164,18 @@ struct PresentationView_Previews: PreviewProvider {
                     secondaryButton: .init(title: "Set Up Later in Settings", action: {
                         
                     })
-                )
+                ) {
+                    
+                }
             }
         
         Text("Presentation")
             .sheet(isPresented: .constant(true)) {
                 PresentationView(
                     title: "What’s New"
-                )
+                ) {
+                    
+                }
             }
         
         Text("Presentation")
@@ -165,7 +185,9 @@ struct PresentationView_Previews: PreviewProvider {
                     primaryButton: .init(title: "Continue", action: {
                         
                     })
-                )
+                ) {
+                    
+                }
             }
         
         Text("Presentation")
@@ -174,7 +196,9 @@ struct PresentationView_Previews: PreviewProvider {
                     systemImage: "person.and.background.dotted",
                     title: "Data & Privacy",
                     subtitle: "This icon appears when an Apple feature asks to use vour personal information.\n\nYou won't see this with every feature since Apple collects this information only when needed to enable features, secure our services or personalise your experience.\n\nApple believes privacy is a fundamental human right, so every Apple product is designed to minimise the collection and use of your data, use on-device processing whenever possible, and provide transparency and control over your information."
-                )
+                ) {
+                    
+                }
             }
     }
 }
