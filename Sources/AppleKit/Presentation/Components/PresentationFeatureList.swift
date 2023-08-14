0fbae88ab01c3cc146d4b0dfc0b782ec5a4dd17a
Linus Långberg <phoqe@phoqe.me>
2023-08-14 12:13:34 +0200

    add scaledmetric to icon

--- Sources/AppleKit/Presentation/Components/PresentationFeatureList.swift
+++ Sources/AppleKit/Presentation/Components/PresentationFeatureList.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 
+@available(iOS 14.0, *)
 struct PresentationFeatureList: View {
     let features: [PresentationFeature]
     
@@ -12,8 +13,20 @@ struct PresentationFeatureList: View {
     }
 }
 
+@available(iOS 14.0, *)
 struct PresentationFeatureList_Previews: PreviewProvider {
     static var previews: some View {
-        PresentationFeatureList(features: [])
+        PresentationFeatureList(features: [
+            PresentationFeature(
+                systemImage: "person.crop.circle",
+                headline: "Profiler",
+                subheadline: "Skapa en profil med uppgifter som födelsedatum och årsinkomst för att beräkna dina avdrag. Dina uppgifter stannar på din enhet och alla beräkningar sker lokalt."
+            ),
+            PresentationFeature(
+                systemImage: "car",
+                headline: "Fordon",
+                subheadline: "Lägg till dina fordon i Inställningar."
+            ),
+        ])
     }
 }
--- Sources/AppleKit/Presentation/Components/PresentationFeatureListItem.swift
+++ Sources/AppleKit/Presentation/Components/PresentationFeatureListItem.swift
@@ -1,15 +1,20 @@
 import SwiftUI
 
+@available(iOS 14.0, *)
 struct PresentationFeatureListItem: View {
     
     let feature: PresentationFeature
+    
+    @ScaledMetric
+    var width = 50
     
     var body: some View {
         HStack(alignment: .center, spacing: 15) {
             if let systemImage = feature.systemImage {
                 Image(systemName: systemImage)
                     .font(.title)
-                .foregroundColor(.accentColor)
+                    .foregroundColor(.accentColor)
+                    .frame(width: width)
             }
             
             if let emoji = feature.emoji {
@@ -31,6 +36,7 @@ struct PresentationFeatureListItem: View {
     }
 }
 
+@available(iOS 14.0, *)
 struct PresentationFeatureListItem_Previews: PreviewProvider {
     static var previews: some View {
         PresentationFeatureListItem(feature: PresentationFeature(subheadline: ""))
